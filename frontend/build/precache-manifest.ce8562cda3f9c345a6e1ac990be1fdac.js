5a0b68043ed6d8ce0309bfc7bd9a8414c8fd5497
Navneet Pannu <navneet.pannu@ticketmaster.com>
2021-08-29 19:01:29 +0530

    adding build

---
+++ frontend/build/precache-manifest.ce8562cda3f9c345a6e1ac990be1fdac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0594d89007b1d22a75708639cd8ae14f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ea8a856fe19217a3737",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7b3dd214557a39184a84",
+    "url": "/static/js/2.a8f65ea1.chunk.js"
+  },
+  {
+    "revision": "fde8a65b8d307872c8c44f6de8ee07de",
+    "url": "/static/js/2.a8f65ea1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ea8a856fe19217a3737",
+    "url": "/static/js/main.11f38119.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
